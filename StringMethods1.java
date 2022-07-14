5d030482c1a67ba65ac6b64e93b74d3064940727
gayatribobbili <108993912+gayatribobbili@users.noreply.github.com>
2022-07-14 15:56:59 +0530

    click

---
+++ StringMethods1.java
@@ -0,0 +1,12 @@
+public class StringMethods1
+{
+public static void main(String[] args)
+{
+String s1="Gayatri";
+//String s2="Girija";
+//String s2="Gayatri";
+String s2="gayatri";
+System.out.println(s1.equals(s2));
+Sytsem.out.println(s1.equalsIgnoreCase(s2));
+}
+}
