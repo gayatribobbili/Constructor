ff01fadb45cc7369afba5722dda61fbb4e5b192e
gayatribobbili <108993912+gayatribobbili@users.noreply.github.com>
2022-07-14 15:56:03 +0530

    Add files via upload

---
+++ StringMethods.java
@@ -0,0 +1,11 @@
+public class StringMethods
+{
+public static void main(String[] args)
+{
+String s1="  Gayatri";
+System.out.println(s1.length());
+System.out.println(s1.isEmpty());
+System.out.println(s1.trim());
+System.out.println(s1.trim().length());
+}
+}
