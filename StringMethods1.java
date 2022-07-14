0ad5f0bd80656d48f0983df634fc09145467d8ed
gayatribobbili <108993912+gayatribobbili@users.noreply.github.com>
2022-07-14 16:06:44 +0530

    Update StringMethods1.java

--- StringMethods1.java
+++ StringMethods1.java
@@ -1,11 +1,13 @@
+import java.utill.Scanner;
 public class StringMethods1
 {
 public static void main(String[] args)
 {
-String s1="Gayatri";
-//String s2="Girija";
-//String s2="Gayatri";
-String s2="gayatri";
+Scanner S = new Scanner(System.in);
+Syatem.out.println("Enter a string:");
+String s1=s.nextLine();
+System.out.println("Enter another String:");
+String s2= s.nextLine();
 System.out.println(s1.equals(s2));
 Sytsem.out.println(s1.equalsIgnoreCase(s2));
 }
