15c215bf87be3840879557c61ac9e83548bb68ac
gayatribobbili <108993912+gayatribobbili@users.noreply.github.com>
2022-07-14 16:18:27 +0530

    click me

---
+++ StringMethods2.java
@@ -0,0 +1,12 @@
+class StringMethods2
+{
+public static void main(String[] args)
+{
+//String s1="Lb";
+//String s2="lm";
+String s1="A";
+String s2="a";
+//System.out.println(s1.compareTo(s2));
+System.out.println(s1.compareToIgnoreCase(s2));
+}
+}
